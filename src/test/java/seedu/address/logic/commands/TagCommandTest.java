f43551c168a275ca83982f8e90cd9fdff93de68f
Ananya Jain <ananyaj1515@gmail.com>
2026-03-18 16:38:32 +0800

    Update TagCommandTest to support new format

--- src/test/java/seedu/address/logic/commands/TagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TagCommandTest.java
@@ -10,18 +10,20 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.StudentId;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.TagType;
 import seedu.address.testutil.PersonBuilder;
 
 public class TagCommandTest {
+
+    private static final StudentId VALID_STUDENT_ID = new StudentId("A1234567X");
+    private static final StudentId UNKNOWN_STUDENT_ID = new StudentId("A9999999N");
 
     private ModelManager model;
 
@@ -30,8 +32,9 @@ public class TagCommandTest {
         model = new ModelManager(new AddressBook(), new UserPrefs());
     }
 
+
     @Test
-    public void constructor_nullIndex_throwsNullPointerException() {
+    public void constructor_nullStudentId_throwsNullPointerException() {
         Map<TagType, Tag> tags = new HashMap<>();
         tags.put(TagType.YEAR, new Tag(TagType.YEAR, "2"));
         assertThrows(NullPointerException.class, () -> new TagCommand(null, tags));
@@ -39,32 +42,32 @@ public class TagCommandTest {
 
     @Test
     public void constructor_nullTags_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new TagCommand(Index.fromOneBased(1), null));
+        assertThrows(NullPointerException.class, () -> new TagCommand(VALID_STUDENT_ID, null));
     }
 
     @Test
-    public void execute_validIndexSingleTag_tagSuccessful() throws Exception {
-        Person person = new PersonBuilder().build();
+    public void execute_validStudentIdSingleTag_tagSuccessful() throws Exception {
+        Person person = new PersonBuilder().withStudentId(VALID_STUDENT_ID.toString()).build();
         model.addPerson(person);
 
         Map<TagType, Tag> tags = new HashMap<>();
         tags.put(TagType.YEAR, new Tag(TagType.YEAR, "2"));
-        new TagCommand(Index.fromOneBased(1), tags).execute(model);
+        new TagCommand(VALID_STUDENT_ID, tags).execute(model);
 
         Person taggedPerson = model.getFilteredPersonList().get(0);
         assertEquals(new Tag(TagType.YEAR, "2"), taggedPerson.getTags().get(TagType.YEAR));
     }
 
     @Test
-    public void execute_validIndexMultipleTags_allTagsApplied() throws Exception {
-        Person person = new PersonBuilder().build();
+    public void execute_validStudentIdMultipleTags_allTagsApplied() throws Exception {
+        Person person = new PersonBuilder().withStudentId(VALID_STUDENT_ID.toString()).build();
         model.addPerson(person);
 
         Map<TagType, Tag> tags = new HashMap<>();
         tags.put(TagType.YEAR, new Tag(TagType.YEAR, "2"));
         tags.put(TagType.MAJOR, new Tag(TagType.MAJOR, "CS"));
         tags.put(TagType.GENDER, new Tag(TagType.GENDER, "M"));
-        new TagCommand(Index.fromOneBased(1), tags).execute(model);
+        new TagCommand(VALID_STUDENT_ID, tags).execute(model);
 
         Map<TagType, Tag> resultTags = model.getFilteredPersonList().get(0).getTags();
         assertEquals(new Tag(TagType.YEAR, "2"), resultTags.get(TagType.YEAR));
@@ -74,24 +77,31 @@ public class TagCommandTest {
 
     @Test
     public void execute_existingTagOverwritten_tagUpdated() throws Exception {
-        Person person = new PersonBuilder().withTags(new Object[]{TagType.YEAR, "1"}).build();
+        Person person = new PersonBuilder()
+                .withStudentId(VALID_STUDENT_ID.toString())
+                .withTags(new Object[]{TagType.YEAR, "1"})
+                .build();
         model.addPerson(person);
 
         Map<TagType, Tag> newTags = new HashMap<>();
         newTags.put(TagType.YEAR, new Tag(TagType.YEAR, "3"));
-        new TagCommand(Index.fromOneBased(1), newTags).execute(model);
+        new TagCommand(VALID_STUDENT_ID, newTags).execute(model);
 
-        assertEquals(new Tag(TagType.YEAR, "3"), model.getFilteredPersonList().get(0).getTags().get(TagType.YEAR));
+        assertEquals(new Tag(TagType.YEAR, "3"),
+                model.getFilteredPersonList().get(0).getTags().get(TagType.YEAR));
     }
 
     @Test
     public void execute_newTagMergedWithExisting_existingTagPreserved() throws Exception {
-        Person person = new PersonBuilder().withTags(new Object[]{TagType.YEAR, "1"}).build();
+        Person person = new PersonBuilder()
+                .withStudentId(VALID_STUDENT_ID.toString())
+                .withTags(new Object[]{TagType.YEAR, "1"})
+                .build();
         model.addPerson(person);
 
         Map<TagType, Tag> newTags = new HashMap<>();
         newTags.put(TagType.MAJOR, new Tag(TagType.MAJOR, "CS"));
-        new TagCommand(Index.fromOneBased(1), newTags).execute(model);
+        new TagCommand(VALID_STUDENT_ID, newTags).execute(model);
 
         Map<TagType, Tag> resultTags = model.getFilteredPersonList().get(0).getTags();
         assertEquals(new Tag(TagType.YEAR, "1"), resultTags.get(TagType.YEAR));
@@ -99,36 +109,35 @@ public class TagCommandTest {
     }
 
     @Test
-    public void execute_indexOutOfBounds_throwsCommandException() {
-        Person person = new PersonBuilder().build();
+    public void execute_unknownStudentId_throwsCommandException() {
+        Person person = new PersonBuilder().withStudentId(VALID_STUDENT_ID.toString()).build();
         model.addPerson(person);
 
         Map<TagType, Tag> tags = new HashMap<>();
         tags.put(TagType.YEAR, new Tag(TagType.YEAR, "2"));
-        TagCommand tagCommand = new TagCommand(Index.fromOneBased(2), tags);
+        TagCommand tagCommand = new TagCommand(UNKNOWN_STUDENT_ID, tags);
 
-        assertThrows(CommandException.class,
-                Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, () -> tagCommand.execute(model));
+        assertThrows(CommandException.class, () -> tagCommand.execute(model));
     }
 
     @Test
     public void execute_emptyList_throwsCommandException() {
         Map<TagType, Tag> tags = new HashMap<>();
         tags.put(TagType.YEAR, new Tag(TagType.YEAR, "2"));
-        TagCommand tagCommand = new TagCommand(Index.fromOneBased(1), tags);
+        TagCommand tagCommand = new TagCommand(VALID_STUDENT_ID, tags);
 
-        assertThrows(CommandException.class,
-                Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, () -> tagCommand.execute(model));
+        assertThrows(CommandException.class, () -> tagCommand.execute(model));
     }
+
 
     @Test
     public void execute_validTag_returnsCorrectSuccessMessage() throws Exception {
-        Person person = new PersonBuilder().build();
+        Person person = new PersonBuilder().withStudentId(VALID_STUDENT_ID.toString()).build();
         model.addPerson(person);
 
         Map<TagType, Tag> tags = new HashMap<>();
         tags.put(TagType.YEAR, new Tag(TagType.YEAR, "2"));
-        CommandResult result = new TagCommand(Index.fromOneBased(1), tags).execute(model);
+        CommandResult result = new TagCommand(VALID_STUDENT_ID, tags).execute(model);
 
         assertTrue(result.getFeedbackToUser().startsWith("Added Tag to Resident"));
     }
