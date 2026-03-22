838d6e5694fd22e949ab9177b15c2ab6848b03c3
La Luz <127034113+dinhcodes@users.noreply.github.com>
2026-03-23 04:46:06 +0800

    Style: Fix checkstyle

--- src/main/java/seedu/address/ui/tab/DemeritRecords.java
+++ src/main/java/seedu/address/ui/tab/DemeritRecords.java
@@ -3,6 +3,9 @@ package seedu.address.ui.tab;
 import javafx.scene.layout.Region;
 import seedu.address.ui.UiPart;
 
+/**
+ * Demerit Records component of the Student Details Tab.
+ */
 public class DemeritRecords extends UiPart<Region> {
 
     private static final String FXML = "DemeritRecords.fxml";
--- src/main/java/seedu/address/ui/tab/Profile.java
+++ src/main/java/seedu/address/ui/tab/Profile.java
@@ -3,6 +3,7 @@ package seedu.address.ui.tab;
 import javafx.scene.layout.Region;
 import seedu.address.ui.UiPart;
 
+/** Student profile component of the Student Details Tab. */
 public class Profile extends UiPart<Region> {
 
     private static final String FXML = "Profile.fxml";
