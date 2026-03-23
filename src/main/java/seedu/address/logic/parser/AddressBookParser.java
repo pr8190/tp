be1cd9e8c8bf5f5d7d6f55c45311d8524dcfe64f
Keng How <khchin0901@gmail.com>
2026-03-23 19:57:55 +0800

    Fix checkstyle issue to pass Github Gradle build CI test

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -12,6 +12,7 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DemeritListCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
@@ -19,7 +20,6 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.TagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.logic.commands.DemeritListCommand;
 
 /**
  * Parses user input.
@@ -29,7 +29,8 @@ public class AddressBookParser {
     /**
      * Used for initial separation of command word and args.
      */
-    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
+    private static final Pattern BASIC_COMMAND_FORMAT =
+            Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
     private static final Logger logger = LogsCenter.getLogger(AddressBookParser.class);
 
     /**
@@ -42,54 +43,53 @@ public class AddressBookParser {
     public Command parseCommand(String userInput) throws ParseException {
         final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
         if (!matcher.matches()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
         }
 
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
 
-        // Note to developers: Change the log level in config.json to enable lower level (i.e., FINE, FINER and lower)
-        // log messages such as the one below.
+        // Note to developers: Change the log level in config.json to enable lower level
+        // (i.e., FINE, FINER and lower) log messages such as the one below.
         // Lower level log messages are used sparingly to minimize noise in the code.
         logger.fine("Command word: " + commandWord + "; Arguments: " + arguments);
 
         switch (commandWord) {
+            case AddCommand.COMMAND_WORD:
+                return new AddCommandParser().parse(arguments);
 
-        case AddCommand.COMMAND_WORD:
-            return new AddCommandParser().parse(arguments);
+            case EditCommand.COMMAND_WORD:
+                return new EditCommandParser().parse(arguments);
 
-        case EditCommand.COMMAND_WORD:
-            return new EditCommandParser().parse(arguments);
+            case DeleteCommand.COMMAND_WORD:
+                return new DeleteCommandParser().parse(arguments);
 
-        case DeleteCommand.COMMAND_WORD:
-            return new DeleteCommandParser().parse(arguments);
+            case ClearCommand.COMMAND_WORD:
+                return new ClearCommand();
 
-        case ClearCommand.COMMAND_WORD:
-            return new ClearCommand();
+            case FindCommand.COMMAND_WORD:
+                return new FindCommandParser().parse(arguments);
 
-        case FindCommand.COMMAND_WORD:
-            return new FindCommandParser().parse(arguments);
+            case ListCommand.COMMAND_WORD:
+                return new ListCommand();
 
-        case ListCommand.COMMAND_WORD:
-            return new ListCommand();
+            case ExitCommand.COMMAND_WORD:
+                return new ExitCommand();
 
-        case ExitCommand.COMMAND_WORD:
-            return new ExitCommand();
+            case HelpCommand.COMMAND_WORD:
+                return new HelpCommand();
 
-        case HelpCommand.COMMAND_WORD:
-            return new HelpCommand();
-
-        case TagCommand.COMMAND_WORD:
-            return new TagCommandParser().parse(arguments);
+            case TagCommand.COMMAND_WORD:
+                return new TagCommandParser().parse(arguments);
 
             case DemeritListCommand.COMMAND_WORD:
                 return new DemeritListCommand();
 
-        default:
-            logger.finer("This user input caused a ParseException: " + userInput);
-            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+            default:
+                logger.finer("This user input caused a ParseException: " + userInput);
+                throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
-
 }
 
--- src/main/java/seedu/address/model/demerit/DemeritRuleCatalogue.java
+++ src/main/java/seedu/address/model/demerit/DemeritRuleCatalogue.java
@@ -7,116 +7,170 @@ import java.util.stream.Collectors;
 /**
  * Static catalogue of demerit rules used by HallLedger.
  *
- * <p>The rules and point tiers are adapted from the Demerit Point Structure (DPS)
- * for Breach of Housing Agreement issued by the Office of Student Affairs,
- * dated 9 January 2026.
+ * <p>The rules and point tiers are adapted from the Demerit Point Structure
+ * (DPS) for Breach of Housing Agreement issued by the Office of Student
+ * Affairs, dated 9 January 2026.
  */
 public final class DemeritRuleCatalogue {
 
-    private DemeritRuleCatalogue() {}
-
     private static final List<DemeritRule> RULES = List.of(
-            new DemeritRule(1,
-                    "Breaches involving or resulting in criminal activity / sexual misconduct / other serious misconduct (including vaping with etomidate or other drugs, sale or distribution of vapes)",
+            new DemeritRule(
+                    1,
+                    "Breaches involving or resulting in criminal activity / sexual misconduct / "
+                            + "other serious misconduct (including vaping with etomidate or "
+                            + "other drugs, sale or distribution of vapes)",
                     25, 25, 25),
 
-            new DemeritRule(2,
-                    "Subletting, attempted subletting and/or accommodating non-residents, including overnight stay, in the room",
+            new DemeritRule(
+                    2,
+                    "Subletting, attempted subletting and/or accommodating non-residents, "
+                            + "including overnight stay, in the room",
                     16, 16, 16),
-            new DemeritRule(3,
+            new DemeritRule(
+                    3,
                     "Throwing of items from height ('Killer Litter')",
                     16, 16, 16),
-            new DemeritRule(4,
-                    "Use of opposite gender’s shower and toilet facilities, including such inappropriate use by visitor(s) of the resident",
+            new DemeritRule(
+                    4,
+                    "Use of opposite gender’s shower and toilet facilities, including such "
+                            + "inappropriate use by visitor(s) of the resident",
                     16, 16, 16),
-            new DemeritRule(5,
+            new DemeritRule(
+                    5,
                     "Duplication of key",
                     16, 16, 16),
-            new DemeritRule(6,
+            new DemeritRule(
+                    6,
                     "Trespassing to room or any hostel facility",
                     16, 16, 16),
-            new DemeritRule(7,
-                    "Smoking, use, or possession of any harmful or imitation tobacco products prohibited by law including electronic vaporisers, shisha, smokeless or chewing tobacco, dissolvable or topical nicotine, and nasal/oral snuff such as Gutkha, Khaini, Zarda etc",
+            new DemeritRule(
+                    7,
+                    "Smoking, use, or possession of any harmful or imitation tobacco products "
+                            + "prohibited by law including electronic vaporisers, shisha, "
+                            + "smokeless or chewing tobacco, dissolvable or topical nicotine, "
+                            + "and nasal/oral snuff such as Gutkha, Khaini, Zarda etc",
                     16, 16, 16),
 
-            new DemeritRule(8,
-                    "Non-compliance with regulations, rules, policies, guidelines, codes of conduct or notices issued by the University or government authorities relating to health and safety",
+            new DemeritRule(
+                    8,
+                    "Non-compliance with regulations, rules, policies, guidelines, codes of "
+                            + "conduct or notices issued by the University or government "
+                            + "authorities relating to health and safety",
                     9, 9, 9),
-            new DemeritRule(9,
-                    "Tampering with communal security/safety measures (including fire-exits, cluster/lift lobby doors, toilet doors and CCTVs, false fire alarm)",
+            new DemeritRule(
+                    9,
+                    "Tampering with communal security/safety measures (including fire-exits, "
+                            + "cluster/lift lobby doors, toilet doors and CCTVs, false fire "
+                            + "alarm)",
                     9, 9, 9),
-            new DemeritRule(10,
+            new DemeritRule(
+                    10,
                     "Passing of hostel keys to another person",
                     9, 9, 9),
-            new DemeritRule(11,
+            new DemeritRule(
+                    11,
                     "Leaving visitors unattended",
                     9, 9, 9),
-            new DemeritRule(12,
+            new DemeritRule(
+                    12,
                     "Unauthorised use of or access to any hostel facility",
                     9, 9, 9),
-            new DemeritRule(13,
-                    "Misconduct (including rude, offensive, disorderly or disruptive behaviour that interferes with the privacy, rights and well-being of others)",
+            new DemeritRule(
+                    13,
+                    "Misconduct (including rude, offensive, disorderly or disruptive behaviour "
+                            + "that interferes with the privacy, rights and well-being of others)",
                     9, 9, 9),
-            new DemeritRule(14,
+            new DemeritRule(
+                    14,
                     "Disorderly conduct under the influence of alcohol",
                     9, 9, 9),
-            new DemeritRule(15,
+            new DemeritRule(
+                    15,
                     "Smoking of cigarettes in hostel",
                     9, 9, 9),
-            new DemeritRule(16,
-                    "Solicitation, sale or promotion of any goods or services using the hostel premises without permission",
+            new DemeritRule(
+                    16,
+                    "Solicitation, sale or promotion of any goods or services using the hostel "
+                            + "premises without permission",
                     9, 9, 9),
-            new DemeritRule(17,
-                    "Misappropriation of common furniture, fixtures, appliances, food and/or personal items belonging to the University or others",
+            new DemeritRule(
+                    17,
+                    "Misappropriation of common furniture, fixtures, appliances, food and/or "
+                            + "personal items belonging to the University or others",
                     9, 9, 9),
 
-            new DemeritRule(18,
-                    "Visit by non-residents of the hostel or visiting a resident of another hostel during quiet hours",
+            new DemeritRule(
+                    18,
+                    "Visit by non-residents of the hostel or visiting a resident of another "
+                            + "hostel during quiet hours",
                     6, 9, 9),
-            new DemeritRule(19,
+            new DemeritRule(
+                    19,
                     "Unauthorised swapping of rooms",
                     6, 9, 9),
-            new DemeritRule(20,
-                    "Allowing stay-over in the room by another resident of the same hostel or stay-over in the room of another resident in the same hostel",
+            new DemeritRule(
+                    20,
+                    "Allowing stay-over in the room by another resident of the same hostel or "
+                            + "stay-over in the room of another resident in the same hostel",
                     6, 9, 9),
-            new DemeritRule(21,
+            new DemeritRule(
+                    21,
                     "Excessive noise during quiet hours",
                     6, 9, 9),
-            new DemeritRule(22,
+            new DemeritRule(
+                    22,
                     "Consuming alcohol in the hostel",
                     6, 9, 9),
-            new DemeritRule(23,
-                    "Possessing alcohol, being in the presence of alcohol consumption in the hostel or ordering alcohol for delivery to the hostel",
+            new DemeritRule(
+                    23,
+                    "Possessing alcohol, being in the presence of alcohol consumption in the "
+                            + "hostel or ordering alcohol for delivery to the hostel",
                     6, 9, 9),
-            new DemeritRule(24,
-                    "Activities that may cause fire, explosion or any kind of hazards (e.g. usage of candles, open flame, 3D printers)",
+            new DemeritRule(
+                    24,
+                    "Activities that may cause fire, explosion or any kind of hazards "
+                            + "(e.g. usage of candles, open flame, 3D printers)",
                     6, 9, 9),
-            new DemeritRule(25,
-                    "Placing of items that cause obstruction of common areas (e.g. passageways, stairs)",
+            new DemeritRule(
+                    25,
+                    "Placing of items that cause obstruction of common areas "
+                            + "(e.g. passageways, stairs)",
                     6, 9, 9),
-            new DemeritRule(26,
-                    "Possession or use of unauthorised electrical appliances, personal refrigerators or coolers / air conditioners without permit from the Management Office",
+            new DemeritRule(
+                    26,
+                    "Possession or use of unauthorised electrical appliances, personal "
+                            + "refrigerators or coolers / air conditioners without permit "
+                            + "from the Management Office",
                     6, 9, 9),
-            new DemeritRule(27,
-                    "Leaving any personal mobility device (PMD) unattended when charging the PMD",
+            new DemeritRule(
+                    27,
+                    "Leaving any personal mobility device (PMD) unattended when charging "
+                            + "the PMD",
                     6, 9, 9),
 
-            new DemeritRule(28,
+            new DemeritRule(
+                    28,
                     "Littering and/or failure to upkeep cleanliness of common areas / room",
                     3, 6, 6),
-            new DemeritRule(29,
+            new DemeritRule(
+                    29,
                     "Keeping of pets",
                     3, 6, 6),
-            new DemeritRule(30,
+            new DemeritRule(
+                    30,
                     "Failure to shut or secure the fire-exit door / cluster door",
                     3, 6, 6),
-            new DemeritRule(31,
+            new DemeritRule(
+                    31,
                     "Leaving lights / fans / appliances switched on when not in room",
                     3, 6, 6),
-            new DemeritRule(32,
+            new DemeritRule(
+                    32,
                     "Non-compliance with prevailing safe management measures in NUS hostels",
                     3, 6, 6)
     );
+
+    private DemeritRuleCatalogue() {}
 
     /**
      * Returns all demerit rules in index order.
