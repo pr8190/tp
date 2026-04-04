f86b7061c240efd691d58f68016e5d8b918ad682
Ananya Jain <ananyaj1515@gmail.com>
2026-04-04 12:06:48 +0800

    Make error messages more friendly

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -13,7 +13,7 @@ import seedu.address.model.person.Person;
  */
 public class Messages {
 
-    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command. Type 'help' to see available commands.";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_EMPTY_ARGUMENT = "Command has empty parameter(s)!";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX =
