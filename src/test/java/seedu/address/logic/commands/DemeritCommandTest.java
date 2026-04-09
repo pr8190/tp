03f662e64f2198a9eff8baaa86e1d3b5be20ffba
Keng How <khchin0901@gmail.com>
2026-04-09 17:11:18 +0800

    Strengthen demerit command tests

--- src/test/java/seedu/address/logic/commands/DemeritCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DemeritCommandTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static seedu.address.logic.Messages.format;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -22,17 +23,14 @@ public class DemeritCommandTest {
         DemeritCommand command = new DemeritCommand(ALICE.getStudentId(), 18, "Stayed over");
 
         CommandResult result = command.execute(model);
-
-        Person updatedPerson = model.getFilteredPersonList().stream()
-                .filter(person -> person.getStudentId().equals(ALICE.getStudentId()))
-                .findFirst()
-                .orElseThrow();
+        Person updatedPerson = findPersonByStudentId(model, ALICE.getStudentId());
 
         assertEquals(6, updatedPerson.getTotalDemeritPoints());
         assertEquals(1, updatedPerson.getDemeritIncidents().size());
+        assertEquals("Stayed over", updatedPerson.getDemeritIncidents().get(0).getRemark());
         assertEquals(String.format(
                 DemeritCommand.MESSAGE_ADD_DEMERIT_SUCCESS,
-                seedu.address.logic.Messages.format(updatedPerson),
+                format(updatedPerson),
                 18,
                 "Visit by non-residents of the hostel or visiting a resident of another hostel during quiet hours",
                 6,
@@ -48,10 +46,7 @@ public class DemeritCommandTest {
         new DemeritCommand(aliceId, 18, "").execute(model);
         new DemeritCommand(aliceId, 18, "").execute(model);
 
-        Person updatedPerson = model.getFilteredPersonList().stream()
-                .filter(person -> person.getStudentId().equals(aliceId))
-                .findFirst()
-                .orElseThrow();
+        Person updatedPerson = findPersonByStudentId(model, aliceId);
 
         assertEquals(24, updatedPerson.getTotalDemeritPoints());
         assertEquals(3, updatedPerson.getDemeritIncidents().size());
@@ -69,5 +64,49 @@ public class DemeritCommandTest {
         DemeritCommand command = new DemeritCommand(new StudentId("A9999999Z"), 18, "");
 
         assertThrows(CommandException.class, () -> command.execute(model));
+    }
+
+    @Test
+    public void execute_invalidRuleIndex_throwsCommandException() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        DemeritCommand command = new DemeritCommand(ALICE.getStudentId(), 999, "");
+
+        assertThrows(CommandException.class, () -> command.execute(model));
+    }
+
+    @Test
+    public void execute_emptyRemark_incidentAddedWithEmptyRemark() throws Exception {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        DemeritCommand command = new DemeritCommand(ALICE.getStudentId(), 18, "");
+
+        command.execute(model);
+
+        Person updatedPerson = findPersonByStudentId(model, ALICE.getStudentId());
+        assertEquals(1, updatedPerson.getDemeritIncidents().size());
+        assertEquals("", updatedPerson.getDemeritIncidents().get(0).getRemark());
+    }
+
+    @Test
+    public void execute_differentRules_trackOffencesIndependently() throws Exception {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        StudentId aliceId = ALICE.getStudentId();
+
+        new DemeritCommand(aliceId, 18, "").execute(model);
+        new DemeritCommand(aliceId, 21, "").execute(model);
+
+        Person updatedPerson = findPersonByStudentId(model, aliceId);
+
+        assertEquals(2, updatedPerson.getDemeritIncidents().size());
+        assertEquals(18, updatedPerson.getDemeritIncidents().get(0).getRuleIndex());
+        assertEquals(21, updatedPerson.getDemeritIncidents().get(1).getRuleIndex());
+        assertEquals(1, updatedPerson.getDemeritIncidents().get(0).getOffenceNumber());
+        assertEquals(1, updatedPerson.getDemeritIncidents().get(1).getOffenceNumber());
+    }
+
+    private Person findPersonByStudentId(Model model, StudentId studentId) {
+        return model.getFilteredPersonList().stream()
+                .filter(person -> person.getStudentId().equals(studentId))
+                .findFirst()
+                .orElseThrow();
     }
 }
