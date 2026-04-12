a702278e05b0adb693cd2c4ea4e33436997d8ab8
Pavithra <pavithra.achar1505@gmail.com>
2026-04-12 14:28:01 +0800

    Change name validation

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters, spaces, hyphens(-) and front slashes(/), and it"
-                    + " should not be blank";
+            "Names should only contain alphanumeric characters, spaces, and the following symbols: . , ' ( ) & @ / -. "
+                    + "It should not be blank";
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "^[A-Za-z0-9]+([ \\-/A-Za-z0-9]+)*$";
+    public static final String VALIDATION_REGEX = "^[A-Za-z0-9][A-Za-z0-9 .,'()&@/-]*$";
 
     public final String fullName;
 
